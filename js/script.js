8889b5642373bce99cfe691f25800634896f4185
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-10-05 08:59:05 -0300

    chore: adicionado comentários

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Chama os elementos do HTML.
 const nameInput = document.getElementById("name");
 const ageInput = document.getElementById("age");
 const emailInput = document.getElementById("email");
@@ -5,10 +6,13 @@ const passwordInput = document.getElementById("password");
 const eye = document.querySelector(".icon-eye");
 const error = document.querySelector(".error");
 const textarea = document.querySelector("textarea");
+const radio = document.querySelectorAll("input[type=radio]");
 
+// Array com os elementos do HTML e os eventos que serão utilizados.
 const inputs = [nameInput, ageInput, emailInput, passwordInput, textarea];
 const events = ["input", "blur"];
 
+// Função responsável por mostrar a senha.
 function togglePassword() {
   eye.classList.toggle("active");
 
@@ -19,46 +23,68 @@ function togglePassword() {
   }
 }
 
+// Função responsável por criar o elemento P de erro.
 function createErrorElement(input, errorMessage) {
-  const errorElement = input.nextElementSibling;
+  const errorElement = input.nextElementSibling; // O nextElementSibling é responsável por retornar o elemento irmão do input, o que vem logo após ele.
 
   if (!errorElement || !errorElement.classList.contains("error")) {
     const errorP = document.createElement("p");
     errorP.classList.add("error");
     errorP.textContent = errorMessage;
+    // O insertBefore é responsável por inserir um elemento antes de outro, recebe dois parâmetros, o primeiro é o elemento que será inserido e o segundo é o elemento que será o próximo irmão do elemento inserido.
+    // O parentNode é responsável por retornar o elemento pai do input.
     input.parentNode.insertBefore(errorP, input.nextSibling);
   } else {
     errorElement.textContent = errorMessage;
   }
 }
 
+// Função responsável por remover o elemento P de erro.
 function removeErrorElement(input) {
   const errorElement = input.nextElementSibling;
+  
   if (errorElement && errorElement.classList.contains("error")) {
     errorElement.remove();
   }
 }
 
+// Função responsável por validar os campos do formulário.
 function handleSubmit(event) {
   event.preventDefault();
+
+  const formData = {};
 
   inputs.forEach((input) => {
     validateInput(input);
   });
 
-  // O método every() testa se todos os elementos do array passam pelo teste implementado pela função atribuída e retorna um valor true ou false.
+  // O método every() é responsável por verificar se todos os elementos do array passam no teste implementado pela função fornecida.
   const allFieldsValid = inputs.every((input) =>
     input.classList.contains("success"),
   );
 
   if (allFieldsValid) {
     alert("Formulário enviado com sucesso!");
+
+    inputs.forEach((input) => {
+      formData[input.id] = input.value;
+
+      radio.forEach((radio) => {
+        if (radio.checked) {
+          formData[radio.name] = radio.id;
+        }
+      });
+
+      input.classList.remove("success");
+    });
+    console.log(formData);
     event.target.reset();
   }
 
   eventsHandler();
 }
 
+// Função responsável por adicionar os eventos aos elementos do HTML.
 function eventsHandler() {
   inputs.forEach((input) => {
     events.forEach((event) => {
@@ -79,6 +105,7 @@ function eventsHandler() {
 
 eventsHandler();
 
+// Função responsável por validar os inputs do formulário.
 function validateInput(input) {
   if (input.value === "") {
     createErrorElement(input, "Campo obrigatório.");
@@ -91,6 +118,7 @@ function validateInput(input) {
   }
 }
 
+// Função responsável por validar o email.
 function validateEmail(input) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -101,6 +129,7 @@ function validateEmail(input) {
   }
 }
 
+// Função responsável por validar a senha.
 function validatePassword() {
   if (passwordInput.value === "") {
     passwordInput.classList.add("error");
@@ -116,7 +145,6 @@ function validatePassword() {
     passwordInput.classList.add("error");
     passwordInput.classList.remove("success");
   } else {
-    error.innerHTML = "";
     passwordInput.classList.remove("error");
     passwordInput.classList.add("success");
   }
